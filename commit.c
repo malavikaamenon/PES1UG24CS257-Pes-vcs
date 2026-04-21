0421c190b6034428ebdb41f24144e4466bd28b64
Malavika <MALAVIKA.MENON2609@GMAIL.COM>
2026-04-21 20:00:21 +0530

    Updated commit.c

--- commit.c
+++ commit.c
@@ -1,18 +1,5 @@
 // commit.c — Commit creation and history traversal
-//
-// Commit object format (stored as text, one field per line):
-//
-//   tree <64-char-hex-hash>
-//   parent <64-char-hex-hash>        ← omitted for the first commit
-//   author <name> <unix-timestamp>
-//   committer <name> <unix-timestamp>
-//
-//   <commit message>
-//
-// Note: there is a blank line between the headers and the message.
-//
-// PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
-// TODO functions:     commit_create
+
 
 #include "commit.h"
 #include "index.h"
